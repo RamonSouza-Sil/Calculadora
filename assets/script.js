cdb2f5ba973dd9a1568c80d286a7f08a71b69c8d
Ramon Souza <ramonsouzasil2004@gmail.com>
2022-07-03 20:12:01 -0300

    subindo js

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,25 @@
 function insert(numb){
- document.getElementById('resultado').innerHTML = numb
+    var numero = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = numero + numb;
+
+
+}
+
+function limpa(){
+    document.getElementById('resultado').innerHTML = "";
+}
+
+function volta(){
+    var resultado = document.getElementById('resultado').innerHTML;
+
+    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length - 1)
+}
+
+function calcular(){
+    var resultado = document.getElementById('resultado').innerHTML;
+
+    if (resultado) {
+        document.getElementById('resultado').innerHTML = eval(resultado);
+    } 
+
 }
